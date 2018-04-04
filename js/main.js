07936ea172663c10b131acd8f69fbb0a69e37b24
Wang <xindi@ISS-LRC-1500-30.iss.lsa.umich.edu>
2018-04-03 21:45:48 -0400

    add gallery

--- js/main.js
+++ js/main.js
@@ -1,12 +1,8 @@
 $(document).ready(function(){
   // $('nav').hide();
-  $('#menu').on('click',function(){
-    $('nav').slideToggle();
-  })
   $('#litebox').on('mouseenter',function(){
     $('#litebox').children().children().attr('src','img/litebox-hover.png');
   }).on('mouseleave',function(){
     $('#litebox').children().children().attr('src','img/litebox.png').slideDown();
   })
-
 })
---
+++ js/mobile-nav.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $('#menu').on('click',function(){
+      $('nav').slideToggle();
+    });
+
+})
