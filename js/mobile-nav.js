bc5e35909184732d9502723e0e06e1d8afaf7a27
xindi-wang <wangcd94@gmail.com>
2018-04-05 01:55:10 -0400

    update

--- js/mobile-nav.js
+++ js/mobile-nav.js
@@ -2,5 +2,10 @@ $(document).ready(function(){
     $('#menu-button').on('click',function(){
       $('nav').slideToggle();
     });
+    $('#logo').on('mouseenter',function(){
+      $('#logo').attr("src","img/icon-small-reverse.png");
+    }).on('mouseleave',function(){
+      $('#logo').attr("src","img/icon-small.png");
+    });
 
 })
