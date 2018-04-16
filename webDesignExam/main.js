ca225a37eb4da2034c5916ee2ff1998a1ad555a2
xindi-wang <wangcd94@gmail.com>
2018-04-16 11:31:57 -0400

    add exam

---
+++ webDesignExam/main.js
@@ -0,0 +1,14 @@
+
+function Hide(){
+
+  var img = document.getElementById('img');
+  img.style.display = 'none';
+
+}
+
+function Show(){
+
+  var img = document.getElementById('img');
+  img.style.display = 'inline-block';
+
+}
