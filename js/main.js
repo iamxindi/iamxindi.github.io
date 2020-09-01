b183ca994e82608d7a0b8179dc8c47d7cf2aa644
Xindi <xindi@Xindis-MacBook-Pro.local>
2020-08-31 21:16:43 -0700

    responsive. added js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
-$(document).ready(function(){
-  // $('nav').hide();
-  $('#litebox').on('mouseenter',function(){
-    $('#litebox').children().children().attr('src','img/litebox-hover.png');
-  }).on('mouseleave',function(){
-    $('#litebox').children().children().attr('src','img/litebox.png').slideDown();
-  })
-})
+// $(document).ready(function(){
+//   // $('nav').hide();
+//   $('#litebox').on('mouseenter',function(){
+//     $('#litebox').children().children().attr('src','img/litebox-hover.png');
+//   }).on('mouseleave',function(){
+//     $('#litebox').children().children().attr('src','img/litebox.png').slideDown();
+//   })
+// })
+// Old JS
+
+console.log("Hi");
+
+function toggleDark() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
